3714b60341c40637240e211fd40d8b0d84f8a570
Unknown <arthur.delamare1@viacesi.fr>
2020-09-04 14:39:32 +0200

    feat(console-container): add newline event

--- client/src/components/console-container.js
+++ client/src/components/console-container.js
@@ -7,11 +7,28 @@ class ConsoleContainer extends LitElement {
     };
   }
 
+  _handleNewLine() {
+    // TODO when ConsoleLine merged : add new component
+    console.log("new line required");
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    const lines = document.querySelectorAll(`div[slot="line"]`);
+    lines.forEach((line) => {
+      line.addEventListener("keyup", () => {
+        // TODO when ConsoleLine merged : put line number into the custom event details
+        line.dispatchEvent(new CustomEvent("newline", { bubbles: true }));
+        console.log("event dispatched");
+      });
+    });
+  }
+
   render() {
     return html`
       <div>
         <h2>${this.language} ${this.language != null ? "c" : "C"}onsole</h2>
-        <div id="lines-wrapper">
+        <div id="lines-wrapper" @newline="${this._handleNewLine}">
           <slot name="line">
             <div>Fallback content (It will be an empty line by default)</div>
           </slot>
